e89d2526e5f24c732bd57c9fd59229f534151af9
Danil Bezus <danil.bezus.db@gmail.com>
2023-06-10 21:26:31 +0300

    Add creating a word with Post and manual route testing

--- server/src/words/words.controller.ts
+++ server/src/words/words.controller.ts
@@ -1,10 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateWordDto } from './dtos/create-word.dto';
+import { WordsService } from './words.service';
 
-@Controller('auth')
+@Controller('words')
 export class WordsController {
+  constructor(private wordsService: WordsService) {}
+
   @Post()
   createWord(@Body() body: CreateWordDto) {
-    console.log(body);
+    this.wordsService.create(
+      body.word,
+      body.definition,
+      body.example,
+      body.pronunciation,
+    );
   }
 }
--- server/src/words/words.service.ts
+++ server/src/words/words.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Word } from './word.entity';
 
 @Injectable()
-export class WordsService {}
+export class WordsService {
+  constructor(@InjectRepository(Word) private repo: Repository<Word>) {}
+
+  create(
+    word: string,
+    definition: string,
+    example: string,
+    pronunciation: string,
+  ) {
+    const newWord = this.repo.create({
+      word,
+      definition,
+      example,
+      pronunciation,
+    });
+
+    return this.repo.save(newWord);
+  }
+}
