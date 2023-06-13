eb7e31341cb0f1136c88dfae7d5651fb43ef3664
Danil Bezus <danil.bezus.db@gmail.com>
2023-06-13 13:01:19 +0300

    Change the function getPage to a class parsingService method

--- server/src/words/parsing.service.ts
+++ server/src/words/parsing.service.ts
@@ -1,25 +1,27 @@
 import { JSDOM } from 'jsdom';
 
-const getPage = (word: string) => {
-  const site = process.env.parsingSite;
-  const request = `${site}${word}`;
-  return fetch(request)
-    .then((response) => response.text())
-    .then((data) => {
-      const dom = new JSDOM(data);
-      const definition = dom.window.document
-        .getElementsByClassName('sense-block pr dsense dsense-noh')[0]
-        .getElementsByClassName('sense-body dsense_b')[0]
-        .getElementsByClassName('def-body ddef_b ddef_b-t')[0]
-        .getElementsByTagName('span')[0].innerHTML;
-      return definition;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+class ParsingService {
+  async getPage(word: string): Promise<string> {
+    const site = process.env.PARSINGSITE;
+    const request = `${site}${word}`;
 
-getPage('word')
+    try {
+      const response = await fetch(request);
+      const data = await response.text();
+      const dom = new JSDOM(data);
+      const definition =
+        dom.window.document.getElementsByClassName('trans dtrans')[0].innerHTML;
+      return definition;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
+
+const parsingService = new ParsingService();
+parsingService
+  .getPage('word')
   .then((definition) => {
     console.log(definition);
   })
