e289a43ec258929a9e6c99a0e071ffc0244bd0d6
Danil Bezus <danil.bezus.db@gmail.com>
2023-06-13 19:46:43 +0300

    Add controller call via bot and fixed the position of the bot files

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,5 +1,5 @@
-import * as TelegramBot from 'node-telegram-bot-api';
-import * as dotenv from 'dotenv';
+import * as TelegramBot from "node-telegram-bot-api";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,9 +7,9 @@ dotenv.config();
 const token = process.env.BOT_TOKEN;
 
 if (!token) {
-    console.error('BOT_TOKEN not found in environment variables');
-    process.exit(1);
-  }
+  console.error("BOT_TOKEN not found in environment variables");
+  process.exit(1);
+}
 
 //bot instance
 const bot = new TelegramBot(token, { polling: true });
@@ -17,8 +17,49 @@ const bot = new TelegramBot(token, { polling: true });
 //message processing
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Привіт, вітаю вас!');
+  bot.sendMessage(chatId, "Привіт, вітаю вас!");
 });
 
+bot.onText(/\/parse (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+
+  if (match && match[1]) {
+    const word = match[1];
+
+    try {
+      const response = await fetch('http://localhost:3000/parsing', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ word }),
+      });
+
+      const result = await response.json();
+
+      await bot.sendMessage(chatId, 'Виберіть один з варіантів:');
+
+      for (const key of Object.keys(result)) {
+        const wordOption = result[key];
+        const message = formatMessage(wordOption);
+        await bot.sendMessage(chatId, message);
+      }
+    } catch (error) {
+      console.error(error);
+      await bot.sendMessage(chatId, 'Помилка при обробці запиту.');
+    }
+  } else {
+    bot.sendMessage(chatId, 'Некоректний формат команди. Введіть команду у форматі /parse <слово>.');
+  }
+});
+
+
+function formatMessage(wordOption: any): string {
+  return `
+Translation: ${wordOption.translation}
+Definition: ${wordOption.definition}
+Example: ${wordOption.example}
+Pronunciation: ${wordOption.pronunciation}
+Parts of Speech: ${wordOption.partsOfSpeech}`.trim();
+}
+
 //bot start
-console.log('The bot is running!');
+console.log("The bot is running!");
