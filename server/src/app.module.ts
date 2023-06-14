8f088f7272daa4358b65ffaa686d1cb5ff815ea7
Danil Bezus <danil.bezus.db@gmail.com>
2023-06-14 20:41:50 +0300

    Move the parsing service to a separate folder

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WordsModule } from './words/words.module';
-import { ParsingModule } from './words/parsing.module';
+import { ParsingModule } from './parsing/parsing.module';
 import { Word } from './words/word.entity';
 import { UserWordsModule } from './user-words/user-words.module';
 import { UserWords } from './user-words/user-words.entity';
